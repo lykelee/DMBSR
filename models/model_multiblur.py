505695176ef5e5d5744e5f0b3bc0707be0aff340
lykelee <90923506+lykelee@users.noreply.github.com>
2024-02-25 22:07:52 +0900

    t

--- models/model_multiblur.py
+++ models/model_multiblur.py
@@ -9,14 +9,14 @@ class ModelMultiblur(ModelPlain):
     # feed L/H data
     # ----------------------------------------
     def feed_data(self, data, need_H=True):
-        self.L = data['L'].to(self.device)  # low-quality image
-        self.kmap = data['kmap'].to(self.device)  # blur kernel
-        self.basis = data['basis'].to(self.device)  # blur kernel
-        self.sf = np.int(data['sf'][0,...].squeeze().cpu().numpy()) # scale factor
-        self.sigma = data['sigma'].to(self.device)  # noise level
-        self.name_img = data['H_path']
+        self.L = data["L"].to(self.device)  # low-quality image
+        self.kmap = data["kmap"].to(self.device)  # blur kernel
+        self.basis = data["basis"].to(self.device)  # blur kernel
+        self.sf = int(data["sf"][0, ...].squeeze().cpu().numpy())  # scale factor
+        self.sigma = data["sigma"].to(self.device)  # noise level
+        self.name_img = data["H_path"]
         if need_H:
-            self.H = data['H'].to(self.device)  # H
+            self.H = data["H"].to(self.device)  # H
 
     # ----------------------------------------
     # feed (L, C) to netG and get E
@@ -25,10 +25,15 @@ class ModelMultiblur(ModelPlain):
         self.E = self.netG(self.L, self.kmap, self.basis, self.sf, self.sigma)
 
     def load(self):
-        load_path_G = self.opt['path']['pretrained_netG']
+        load_path_G = self.opt["path"]["pretrained_netG"]
         if load_path_G is not None:
-            print('Loading model for G [{:s}] ...'.format(load_path_G))
-            self.load_network(load_path_G, self.netG.module.p, strict=self.opt_train['G_param_strict'], param_key='params')
-        
-        #for k,v in self.netG.module.p.named_parameters():
-            #v.requires_grad = False
+            print("Loading model for G [{:s}] ...".format(load_path_G))
+            self.load_network(
+                load_path_G,
+                self.netG.module.p,
+                strict=self.opt_train["G_param_strict"],
+                param_key="params",
+            )
+
+        # for k,v in self.netG.module.p.named_parameters():
+        # v.requires_grad = False
--- models/model_plain4.py
+++ models/model_plain4.py
@@ -9,13 +9,13 @@ class ModelPlain4(ModelPlain):
     # feed L/H data
     # ----------------------------------------
     def feed_data(self, data, need_H=True):
-        self.L = data['L'].to(self.device)  # low-quality image
-        self.k = data['k'].to(self.device)  # blur kernel
-        self.sf = np.int(data['sf'][0,...].squeeze().cpu().numpy()) # scale factor
-        self.sigma = data['sigma'].to(self.device)  # noise level
-        self.name_img = data['H_path']
+        self.L = data["L"].to(self.device)  # low-quality image
+        self.k = data["k"].to(self.device)  # blur kernel
+        self.sf = int(data["sf"][0, ...].squeeze().cpu().numpy())  # scale factor
+        self.sigma = data["sigma"].to(self.device)  # noise level
+        self.name_img = data["H_path"]
         if need_H:
-            self.H = data['H'].to(self.device)  # H
+            self.H = data["H"].to(self.device)  # H
 
     # ----------------------------------------
     # feed (L, C) to netG and get E
@@ -24,7 +24,12 @@ class ModelPlain4(ModelPlain):
         self.E = self.netG(self.L, self.k, self.sf, self.sigma)
 
     def load(self):
-        load_path_G = self.opt['path']['pretrained_netG']
+        load_path_G = self.opt["path"]["pretrained_netG"]
         if load_path_G is not None:
-            print('Loading model for G [{:s}] ...'.format(load_path_G))
-            self.load_network(load_path_G, self.netG.module.p, strict=self.opt_train['G_param_strict'], param_key='params')
+            print("Loading model for G [{:s}] ...".format(load_path_G))
+            self.load_network(
+                load_path_G,
+                self.netG.module.p,
+                strict=self.opt_train["G_param_strict"],
+                param_key="params",
+            )
